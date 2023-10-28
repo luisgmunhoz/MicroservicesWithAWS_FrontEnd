4547947a82f40f5a5c2b29195d3cae662209d825
Luis Guilherme de Souza Munhoz <luisguilherme.sm@hotmail.com>
2023-10-28 13:20:50 -0300

    Changing cognito configs

--- scripts/cognito.js
+++ scripts/cognito.js
@@ -1,34 +1,29 @@
-const config={
-    cognito:{
-        identityPoolId:"ap-southeast-2_2Dm0JvSbL",
-        cognitoDomain:"hotel.auth.ap-southeast-2.amazoncognito.com",
-        appId:"104q5t9t9moj02ga6ufi39j6ct"
-    }
-}
+const config = {
+    cognito: {
+        identityPoolId: process.env.IDENTITY_POOL_ID,
+        cognitoDomain: process.env.COGNITO_DOMAIN,
+        appId: process.env.APP_ID,
+    },
+};
 
-var cognitoApp={
-    auth:{},
-    Init: function()
-    {
-
+var cognitoApp = {
+    auth: {},
+    Init: function () {
         var authData = {
-            ClientId : config.cognito.appId,
-            AppWebDomain : config.cognito.cognitoDomain,
-            TokenScopesArray : ['email', 'openid','profile'],
-            RedirectUriSignIn : 'http://localhost:8080/hotel/',
-            RedirectUriSignOut : 'http://localhost:8080/hotel/',
-            UserPoolId : config.cognito.identityPoolId, 
-            AdvancedSecurityDataCollectionFlag : false,
-                Storage: null
+            ClientId: config.cognito.appId,
+            AppWebDomain: config.cognito.cognitoDomain,
+            TokenScopesArray: ["email", "openid", "profile"],
+            RedirectUriSignIn: "http://localhost:8080/",
+            RedirectUriSignOut: "http://localhost:8080/",
+            UserPoolId: config.cognito.identityPoolId,
+            AdvancedSecurityDataCollectionFlag: false,
+            Storage: null,
         };
 
         cognitoApp.auth = new AmazonCognitoIdentity.CognitoAuth(authData);
         cognitoApp.auth.userhandler = {
-            onSuccess: function(result) {
-              
-            },
-            onFailure: function(err) {
-            }
+            onSuccess: function (result) {},
+            onFailure: function (err) {},
         };
-    }
-}
+    },
+};
